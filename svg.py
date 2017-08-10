5593c0834c79cc6cd722c7b5612cbc4b1801ce3a
vitaminwater <stantin04@gmail.com>
2017-08-10 12:34:56 +0200

    variable speed

--- svg.py
+++ svg.py
@@ -3,16 +3,15 @@ from turtle import *
 import numpy as np
 from svgpathtools import svg2paths
 
-paths, attributes = svg2paths('smiley2.svg')
+paths, attributes = svg2paths('smiley.svg')
 
 color('red')
 for _, path in enumerate(paths):
     # list path points
     for seg in path:
         penup()
-        for step in np.arange(0.0,1.0,0.01):
+        for step in np.arange(0.0,1.0,1/seg.length()):
             p = seg.point(step)
-            print(p.real, p.imag)
             goto(p.real * 3 - 200, -p.imag * 3 + 200)
             if step == 0:
                 pendown()
