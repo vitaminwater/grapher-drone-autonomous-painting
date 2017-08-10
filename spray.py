d7b2ae45c1f23b88fd50ea340230a5565cf74b48
vitaminwater <stantin04@gmail.com>
2017-08-10 10:25:56 +0000

    exit sequence in spray.py

--- spray.py
+++ spray.py
@@ -6,9 +6,12 @@ banksy = connect('/dev/ttyAMA0', wait_ready=True, baud=921600)
 print "connected"
 
 while True:
+    if banksy.channels['6'] > 1500: break
     banksy.channels.overrides['8'] = 1934
     time.sleep(5)
     banksy.channels.overrides['8'] = 1094
     time.sleep(5)
+
+banksy.channels.overrides['8'] = None
 
 print "Ready"
