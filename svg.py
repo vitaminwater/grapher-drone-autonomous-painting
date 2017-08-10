3fa0dc2d6225c6124cf3db1bb302ee384f0ae4c8
vitaminwater <stantin04@gmail.com>
2017-08-10 11:58:17 +0200

    better path tracing

--- svg.py
+++ svg.py
@@ -3,15 +3,17 @@ from turtle import *
 import numpy as np
 from svgpathtools import svg2paths
 
-paths, attributes = svg2paths('smiley.svg')
+paths, attributes = svg2paths('smiley2.svg')
 
-color('red', 'yellow')
-begin_fill()
+color('red')
 for _, path in enumerate(paths):
     # list path points
-    for step in np.arange(0.0,1.0,0.01):
-        p = path.point(step)
-        print(p.real, p.imag)
-        goto(p.real, -p.imag)
-end_fill()
+    for seg in path:
+        penup()
+        for step in np.arange(0.0,1.0,0.01):
+            p = seg.point(step)
+            print(p.real, p.imag)
+            goto(p.real * 3 - 200, -p.imag * 3 + 200)
+            if step == 0:
+                pendown()
 done()
